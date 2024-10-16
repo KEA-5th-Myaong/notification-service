9b55caddd48c0efaf29c1d66e1dee39e80120b9a
Sujin Kang <1214sujin@naver.com>
2024-10-16 20:07:33 +0900

    :sparkles: feature: 데이터 초기화 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into "notification" (noti_id, member_id, title, content, url, is_read, created_at, updated_at) values
+	(1, 5, 'Totam tenetur tempora quia.', 'Minima perferendis odit quo.', 'http://gimseogim.net/mainlogin.asp', false, now(), now()),
+	(2, 5, 'Repellendus asperiores possimus explicabo.', 'Repellendus error deserunt sint corrupti quasi.', 'https://www.jusighoesa.com/taghome.html', false, now(), now()),
+	(3, 5, 'Fugiat quasi facere velit sapiente qui.', 'Natus explicabo vel ipsam.', 'http://ju.kr/app/tagcategory.php', true, now(), now()),
+	(4, 5, 'Ab quidem cum dolor similique at culpa.', 'Earum aperiam velit exercitationem.', 'http://yu.com/list/categories/listprivacy.php', true, now(), now()),
+	(5, 5, 'Porro asperiores totam porro ab harum veniam.', 'Aliquam dolore perspiciatis odio.', 'http://www.coebagcoe.kr/tag/tagssearch.html', false, now(), now());
+
