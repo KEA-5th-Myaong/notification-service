2454ecc2193fb9c7617313ae96b4a176cfc92cfa
Sujin Kang <1214sujin@naver.com>
2024-10-18 19:58:24 +0900

    🔨 fix: H2의 환경을 MySQL과 일치

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into "notification" (noti_id, member_id, title, content, url, is_read, created_at, updated_at) values
+insert into `notification` (noti_id, member_id, title, content, url, is_read, created_at, updated_at) values
 	(1, 5, 'Totam tenetur tempora quia.', 'Minima perferendis odit quo.', 'http://gimseogim.net/mainlogin.asp', false, now(), now()),
 	(2, 5, 'Repellendus asperiores possimus explicabo.', 'Repellendus error deserunt sint corrupti quasi.', 'https://www.jusighoesa.com/taghome.html', false, now(), now()),
 	(3, 5, 'Fugiat quasi facere velit sapiente qui.', 'Natus explicabo vel ipsam.', 'http://ju.kr/app/tagcategory.php', true, now(), now()),
