56c25987049f6e1b709ffa4ba81483c661e76836
chogh824 <chogh824@naver.com>
2024-11-25 03:35:14 +0900

    feature: 알림 생성 로직 일부 수정과 그에 따른 feignclient 로직 구현 + TODO list 정리

--- src/main/java/myaong/popolog/notificationservice/service/NotificationService.java
+++ src/main/java/myaong/popolog/notificationservice/service/NotificationService.java
@@ -7,6 +7,7 @@ import myaong.popolog.notificationservice.dto.request.NotificationRequest;
 import myaong.popolog.notificationservice.dto.response.NotificationDto;
 import myaong.popolog.notificationservice.dto.response.NotificationResponse;
 import myaong.popolog.notificationservice.entity.Notification;
+import myaong.popolog.notificationservice.feign.client.MemberServiceFeignClient;
 import myaong.popolog.notificationservice.repository.NotificationRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,17 +21,23 @@ import java.util.stream.Collectors;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final MemberServiceFeignClient memberServiceFeignClient;
 
-    public NotificationService(NotificationRepository notificationRepository) {
+    public NotificationService(NotificationRepository notificationRepository, MemberServiceFeignClient memberServiceFeignClient) {
         this.notificationRepository = notificationRepository;
+        this.memberServiceFeignClient = memberServiceFeignClient;
     }
-
+    // TODO: 각 다른 유형의 알림을 전송받는 로직이 필요
     // 알림 생성
     @Transactional
-    public void createNotification(NotificationRequest request, NotificationType type, Long memberId) {
+    public void createNotification(NotificationRequest request, NotificationType type, Long senderMemberId) {
+        // 알림 제목에 nickname을 받기 위한 로직
+        String senderNickname = fetchSenderNicknameById(senderMemberId);
+        String title = determineTitle(type, senderNickname);
+
         Notification notification = Notification.builder()
                 .memberId(request.getMemberId())
-                .title(request.getTitle())
+                .title(title)
                 .content(request.getContent())
                 .url(request.getUrl())
                 .type(type)
@@ -38,6 +45,38 @@ public class NotificationService {
                 .build();
 
         notificationRepository.save(notification);
+    }
+
+    // TODO: Member-service에 Nickname 조회하는 로직 구현 필요
+    // TODO: Member-service에 feign 폴더를 만들어 feign/service, feign/controller를 만들어 그 곳에 로직을 구현할 예정
+    //senderMemberId로 사용자 nickname 조회
+    private String fetchSenderNicknameById(Long senderMemberId) {
+        try {
+            return memberServiceFeignClient.getNicknameById(senderMemberId);
+        } catch (Exception e) {
+            // 예외 처리: nickname 조회 실패 시 기본값 사용
+            throw new ApiException(ApiCode.DB_ERROR);
+        }
+    }
+
+    // 알림의 유형에 따른 제목 생성
+    private String determineTitle(NotificationType type, String senderNickname) {
+        switch (type) {
+            case COMMENT:
+                return String.format("%s님께서 회원님의 게시글에 새로운 댓글을 달았습니다.", senderNickname);
+            case REPLY:
+                return String.format("%s님께서 회원님의 댓글에 답글을 달았습니다.", senderNickname);
+            case LIKE:
+                return String.format("%s님께서 회원님의 게시글을 좋아합니다.", senderNickname);
+            case FOLLOW:
+                return String.format("%s님께서 회원님을 팔로우하였습니다.", senderNickname);
+            case NOTICE:
+                return "새 공지가 등록되었습니다.";
+            case INQUIRY_REPLY:
+                return "문의에 대한 답변이 도착했습니다.";
+            default:
+                return "새로운 알림이 있습니다.";
+        }
     }
 
 
